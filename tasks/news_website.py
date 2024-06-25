c52a7441bc1bb9ea2882fd7906c5dd2123809fc5
Melissa Zucet Rodriguez Vasquez <rodriguez.m106411@allied.global>
2024-06-25 17:59:48 -0600

    dependencies

--- tasks/news_website.py
+++ tasks/news_website.py
@@ -11,11 +11,13 @@ from datetime import datetime, timedelta
 from RPA.Browser.Selenium import Selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import geckodriver_autoinstaller
-#from get_gecko_driver import GetGeckoDriver
+#import geckodriver_autoinstaller
+from get_gecko_driver import GetGeckoDriver
 from selenium.common.exceptions import ElementNotVisibleException,NoSuchElementException,StaleElementReferenceException,ElementNotInteractableException
 
-geckodriver_autoinstaller.install()
+#geckodriver_autoinstaller.install()
+firefox_driver = GetGeckoDriver()
+firefox_driver.install()
 
 class NewsWebsiteAutomation:
 
